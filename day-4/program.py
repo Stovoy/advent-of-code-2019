2be52def4688bb898197a375df7000da1abd7167
Steve Mostovoy <stevemostovoysm@gmail.com>
2019-12-03 22:20:35 -0800

    Simplify day 4.

--- day-4/program.py
+++ day-4/program.py
@@ -3,13 +3,13 @@ with open('input.txt') as input_file:
 
 
 def p1(s):
-    pairs = [(a, b) for a, b in zip(s, s[1:])]
+    pairs = list(zip(s, s[1:]))
     return any(a == b for a, b in pairs) and all(b >= a for a, b in pairs)
 
 
 def p2(s):
-    pairs = [(a, b) for a, b in zip(s, s[1:])]
-    match = [False] + [a == b for a, b in zip(s, s[1:])] + [False]
+    pairs = list(zip(s, s[1:]))
+    match = [False] + [a == b for a, b in pairs] + [False]
     return any(not a and b and not c for a, b, c in zip(match, match[1:], match[2:])) and all(
         b >= a for a, b in pairs)
 
