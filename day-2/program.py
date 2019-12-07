d0400a2ec47e734ad253fd2f8720a8bf4c0fc070
Steve Mostovoy <stevemostovoysm@gmail.com>
2019-12-06 23:53:47 -0800

    Refactor day 2 and day 5 to use intcode computer

--- day-2/program.py
+++ day-2/program.py
@@ -1,41 +1,27 @@
+from intcode import Runtime
+
 with open('input.txt') as input_file:
     lines = input_file.readlines()
 
 
-def run_program(program, noun, verb):
+def run_noun_verb(program, noun, verb):
+    program = program[:]
     program[1] = noun
     program[2] = verb
-
-    i = 0
-    while i < len(program):
-        opcode = program[i]
-        if opcode == 1:
-            i0 = program[i + 1]
-            i1 = program[i + 2]
-            dest = program[i + 3]
-            program[dest] = program[i0] + program[i1]
-        elif opcode == 2:
-            i0 = program[i + 1]
-            i1 = program[i + 2]
-            dest = program[i + 3]
-            program[dest] = program[i0] * program[i1]
-        elif opcode == 99:
-            break
-        i += 4
-
+    runtime = Runtime(program)
+    runtime.run()
     return program[0]
 
 
 def find_noun_verb(program, desired_output):
     for noun in range(0, 100):
         for verb in range(0, 100):
-            result = run_program(program[:], noun, verb)
-            if result == desired_output:
+            if run_noun_verb(program, noun, verb) == desired_output:
                 return noun, verb
 
 
 line = lines[0]
 program = list(map(int, line.split(",")))
-print(run_program(program[:], 12, 2))
-noun, verb = find_noun_verb(program[:], 19690720)
+print(run_noun_verb(program, 12, 2))
+noun, verb = find_noun_verb(program, 19690720)
 print(100 * noun + verb)
--- day-5/program.py
+++ day-5/program.py
@@ -1,81 +1,16 @@
+from intcode import Runtime
+
 with open('input.txt') as input_file:
     lines = input_file.readlines()
 
 
-def parse_opcode(opcode):
-    return opcode[2] == "0", opcode[1] == "0", opcode[0] == "0", int(opcode[3:5])
-
-
-def parse_param(program, index, is_positional):
-    return program[program[index]] if is_positional else program[index]
-
-
-def write_to(program, address, value):
-    program[address] = value
-
-
-def run_program(program, input_value):
-    i = 0
-    while i < len(program):
-        opcode = program[i]
-        opcode = str(opcode)
-        while len(opcode) < 5:
-            opcode = "0" + opcode
-        p0_positional, p1_positional, p2_positional, instruction = parse_opcode(opcode)
-        if instruction == 1:
-            p0 = parse_param(program, i + 1, p0_positional)
-            p1 = parse_param(program, i + 2, p1_positional)
-            write_to(program, program[i + 3], p0 + p1)
-            i += 4
-        elif instruction == 2:
-            p0 = parse_param(program, i + 1, p0_positional)
-            p1 = parse_param(program, i + 2, p1_positional)
-            write_to(program, program[i + 3], p0 * p1)
-            i += 4
-        elif instruction == 3:
-            write_to(program, program[i + 1], input_value)
-            i += 2
-        elif instruction == 4:
-            p0 = parse_param(program, i + 1, p0_positional)
-            print(p0)
-            i += 2
-        elif instruction == 5:
-            p0 = parse_param(program, i + 1, p0_positional)
-            p1 = parse_param(program, i + 2, p1_positional)
-            if p0 != 0:
-                i = p1
-            else:
-                i += 3
-        elif instruction == 6:
-            p0 = parse_param(program, i + 1, p0_positional)
-            p1 = parse_param(program, i + 2, p1_positional)
-            if p0 == 0:
-                i = p1
-            else:
-                i += 3
-        elif instruction == 7:
-            p0 = parse_param(program, i + 1, p0_positional)
-            p1 = parse_param(program, i + 2, p1_positional)
-            if p0 < p1:
-                write_to(program, program[i + 3], 1)
-            else:
-                write_to(program, program[i + 3], 0)
-            i += 4
-        elif instruction == 8:
-            p0 = parse_param(program, i + 1, p0_positional)
-            p1 = parse_param(program, i + 2, p1_positional)
-            if p0 == p1:
-                write_to(program, program[i + 3], 1)
-            else:
-                write_to(program, program[i + 3], 0)
-            i += 4
-        elif instruction == 99:
-            break
-        else:
-            raise ValueError(opcode)
-
-
 line = lines[0]
 program = list(map(int, line.split(",")))
-run_program(program[:], 1)
-run_program(program[:], 5)
+
+runtime = Runtime(program[:], [1])
+runtime.run()
+print(runtime.outputs[-1])
+
+runtime = Runtime(program[:], [5])
+runtime.run()
+print(runtime.outputs[-1])
