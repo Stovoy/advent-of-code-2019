0a10a47d28fd5e70b83709f6c77b5e358f182ae9
Steve Mostovoy <stevemostovoysm@gmail.com>
2019-12-09 23:10:16 -0800

    Day 10.

---
+++ day-10/program.py
@@ -0,0 +1,60 @@
+import math
+
+with open('input.txt') as input_file:
+    lines = input_file.readlines()
+
+asteroids = {}
+y = 0
+for line in lines:
+    x = 0
+    for c in line:
+        if c == '#':
+            asteroids[(x, y)] = True
+        x += 1
+    y += 1
+
+
+def check_asteroids(asteroids, position):
+    seen = {}
+    for other_position in asteroids.keys():
+        if position == other_position:
+            continue
+        d_x, d_y = other_position[0] - position[0], other_position[1] - position[1]
+        angle = math.atan2(d_x, d_y)
+        info = other_position, (d_x ** 2 + d_y ** 2)
+        if angle in seen:
+            seen[angle].append(info)
+        else:
+            seen[angle] = [info]
+    return seen
+
+
+asteroid_los_counts = []
+for position, asteroid in asteroids.items():
+    count_same_angle = len(check_asteroids(asteroids, position))
+    asteroid_los_counts.append((position, count_same_angle))
+
+position, count = max(asteroid_los_counts, key=lambda x: x[1])
+print(count)
+
+asteroids_destroyed = 0
+while True:
+    asteroid_info = check_asteroids(asteroids, position)
+    for angle in asteroid_info:
+        asteroid_info[angle] = sorted(asteroid_info[angle], key=lambda x: x[1])
+
+    angles = []
+    for asteroids_at_angle in asteroid_info.values():
+        asteroid = asteroids_at_angle[0][0]
+        d_x = asteroid[0] - position[0]
+        d_y = asteroid[1] - position[1]
+        angles.append((math.atan2(d_x, d_y), asteroid))
+
+    clockwise_asteroids = map(lambda v: v[1], sorted(angles)[::-1])
+
+    for asteroid in clockwise_asteroids:
+        asteroids_destroyed += 1
+        if asteroids_destroyed == 200:
+            print(asteroid[0] * 100 + asteroid[1])
+            break
+        del asteroids[(asteroid[0], asteroid[1])]
